1c09f8cad544dcc18a2e419916e40ff36ed37cbb
Robert McIntosh <rmcintosh@remabledesigns.com>
2021-07-28 12:45:08 -0400

    Update ExtendedCombatHud.js
    Fixed dialog drag to use new position.

--- scripts/ExtendedCombatHud.js
+++ scripts/ExtendedCombatHud.js
@@ -1168,6 +1168,10 @@ class ECHDiceRoller {
         left: offset.left - $(document).scrollLeft(),
       }).addClass('ech-highjack-window');
 
+      // Update dialog with new position data for dragging.
+      dialog.position.left = offset.left - $(document).scrollLeft();
+      dialog.position.top =  offset.top - $(document).scrollTop() - (dialog.position.height / 2);
+
       // If Dialog allows you to select Modifier, use modifier from ability modifier by default
       if (!html.find('select[name="ability"]'))
         html.find('select[name="ability"]').val($element.data('modifier'));
